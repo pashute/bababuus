1f8c6f8bc9e1306081d150ce127cee6ca0697339
pashute <pashute@gmail.com>
2019-09-25 22:32:21 +0300

    first time

---
+++ lib/styles/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 26.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const String fontNameDefault = "notSetYet"; // 'MontSerrat'
+
+const AppBarTextStyle = TextStyle(
+  //fontFamily: fontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: MediumTextSize,
+  color: Colors.white 
+  );
+
